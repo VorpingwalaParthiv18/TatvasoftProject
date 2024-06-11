f20591eedd9b8aec232eb26a5aab6a6083955c83
VorpingwalaParthiv18 <vorpingwalaparthiv@gmail.com>
2024-06-11 16:24:05 +0530

    TatvasoftProjectCode

---
+++ BackendCode/CLPWEBAPI/Data_Access_Layer/Migrations/20240611094659_MissionApp.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Data_Access_Layer.Migrations
+{
+    /// <inheritdoc />
+    public partial class MissionApp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MissionApplication",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    MissionId = table.Column<int>(type: "integer", nullable: false),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    AppliedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Status = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    ModifiedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MissionApplication", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MissionApplication_Missions_MissionId",
+                        column: x => x.MissionId,
+                        principalTable: "Missions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MissionFavourites",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    MissionId = table.Column<int>(type: "integer", nullable: false),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    ModifiedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MissionFavourites", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MissionRating",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    MissionId = table.Column<int>(type: "integer", nullable: false),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    Rating = table.Column<int>(type: "integer", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    ModifiedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MissionRating", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MissionApplication_MissionId",
+                table: "MissionApplication",
+                column: "MissionId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MissionApplication");
+
+            migrationBuilder.DropTable(
+                name: "MissionFavourites");
+
+            migrationBuilder.DropTable(
+                name: "MissionRating");
+        }
+    }
+}
---
+++ BackendCode/CLPWEBAPI/Data_Access_Layer/Migrations/20240611101043_missionapp_3.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data_Access_Layer.Migrations
+{
+    /// <inheritdoc />
+    public partial class missionapp_3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MissionApplication_Missions_MissionId",
+                table: "MissionApplication");
+
+            migrationBuilder.DropIndex(
+                name: "IX_MissionApplication_MissionId",
+                table: "MissionApplication");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_MissionApplication_MissionId",
+                table: "MissionApplication",
+                column: "MissionId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MissionApplication_Missions_MissionId",
+                table: "MissionApplication",
+                column: "MissionId",
+                principalTable: "Missions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
